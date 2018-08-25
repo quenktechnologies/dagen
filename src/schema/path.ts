0d09f6719a50267e87d80fa1f4cb79d557f9f538
Lasana Murray <lmurray@quenk.com>
2018-08-25 15:47:43 -0400

    refactor: add schema/path module.

---
+++ src/schema/path.ts
@@ -0,0 +1,20 @@
+import * as json from '@quenk/noni/lib/data/json';
+import { set } from 'property-seek';
+import { match } from '@quenk/noni/lib/control/match';
+import { flip, cons } from '@quenk/noni/lib/data/function';
+import { map } from '@quenk/noni/lib/data/array';
+import { reduce, flatten } from '@quenk/noni/lib/data/record';
+
+/**
+ * expand the paths of a JSON value authored in short-form recursively.
+ */
+export const expand = (val: json.Value): json.Value => <json.Value>match(val)
+    .caseOf(Array, expandArray)
+    .caseOf(Object, expandObject)
+    .orElse(cons(val))
+    .end();
+
+export const expandArray = (flip(map)(expand));
+
+export const expandObject = (o: json.Object): json.Object =>
+    reduce(flatten(o), {}, (p, c, k) => set(k, c, p));
