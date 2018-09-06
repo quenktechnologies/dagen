39c0d46202f08f5b113052a75fd7e5fc43a99531
Lasana Murray <lmurray@quenk.com>
2018-09-06 17:17:50 -0400

    refactor(schema/checks): add checks module.

--- src/schema/checks/index.ts
+++ src/schema/checks/index.ts
@@ -2,19 +2,127 @@
  * The checks module provides functions for validating a JSON document
  * against a Data Document Schema using the Checks Extension features.
  */
-import * as api from './check';
+import * as json from '@quenk/noni/lib/data/json';
+import * as arrays from '@quenk/preconditions/lib/array';
+import * as objects from '@quenk/preconditions/lib/object';
+import * as strings from '@quenk/preconditions/lib/string';
+import * as numbers from '@quenk/preconditions/lib/number';
+import * as booleans from '@quenk/preconditions/lib/boolean';
 import { match } from '@quenk/noni/lib/control/match';
-import { Schema } from '../';
-import { Check } from './check';
+import { map, reduce } from '@quenk/noni/lib/data/record';
+import { Precondition, and, or, every, fail, identity } from '@quenk/preconditions';
+import {
+    TYPE_OBJECT,
+    TYPE_ARRAY,
+    TYPE_SUM,
+    TYPE_STRING,
+    TYPE_NUMBER,
+    TYPE_BOOLEAN,
+    Schema,
+    ObjectType,
+    ArrayType,
+    SumType
+} from '../';
+
 
 /**
- * fromSchema rewrites a schema to a chain of Checks.
- *
- * The result can later be interpreted.
+ * Check is a Precondition that accepts some JSON value as input.
  */
-export const fromSchema = (s: Schema): Check => <Check>match(s)
-    .caseOf({ type: 'object' }, fromObject)
+export type Check<B> = Precondition<json.Value, B>;
+
+const objectShapeWithBothProps = {
+
+    type: TYPE_OBJECT,
+
+    properties: Object,
+
+    additionalProperties: Object
+
+};
+
+const objectShapeWithProps = {
+
+    type: TYPE_OBJECT,
+
+    properties: Object
+
+}
+
+const objectShapeWithAdditionalProps = {
+
+    type: TYPE_OBJECT,
+
+    additionalProperties: Object
+
+}
+
+const arrayShape = {
+
+    type: TYPE_ARRAY,
+
+    items: Object
+
+}
+
+const sumShape = {
+
+    type: TYPE_SUM,
+
+    variants: Object
+
+}
+
+const stringShape = {
+
+    type: TYPE_STRING
+
+}
+
+const numberShape = {
+
+    type: TYPE_NUMBER
+
+}
+
+const booleanShape = {
+
+    type: TYPE_BOOLEAN
+
+}
+
+const externalShape = {
+
+    type: String
+
+}
+
+/**
+ * fromSchema rewrites a Schema to a chain of Checks.
+ */
+export const fromSchema = <B>(s: Schema): Check<B> => <Check<B>>match(s)
+    .caseOf(objectShapeWithBothProps, fromMapObject)
+    .caseOf(objectShapeWithProps, fromObject)
+    .caseOf(objectShapeWithAdditionalProps, fromMap)
+    .caseOf(arrayShape, fromArray)
+    .caseOf(sumShape, fromSum)
+    .caseOf(stringShape, () => strings.isString)
+    .caseOf(numberShape, () => numbers.isNumber)
+    .caseOf(booleanShape, () => booleans.isBoolean)
+    .caseOf(externalShape, () => identity)
     .end();
 
-const fromObject = (): Check => api.typeOf('object', '');
+const fromObject = ({ properties }: ObjectType) =>
+    and(objects.isObject, objects.disjoint(map(properties, fromSchema)));
 
+const fromMap = ({ additionalProperties }: ObjectType) =>
+    and(objects.isObject, objects.map(fromSchema(additionalProperties)));
+
+const fromMapObject = ({ properties, additionalProperties }: ObjectType) =>
+    every(objects.isObject, objects.disjoint(map(properties, fromSchema)),
+        objects.map(fromSchema(additionalProperties)));
+
+const fromArray = ({ items }: ArrayType) =>
+    and(arrays.isArray, arrays.map(fromSchema(items)));
+
+const fromSum = ({ variants }: SumType) =>
+    reduce(map(variants, fromSchema), fail(''), or)
--- test/schema/checks/index_test.ts
+++ test/schema/checks/index_test.ts
@@ -1,19 +1,246 @@
 import * as must from 'must/register';
-import { Functor } from '@quenk/noni/lib/data/functor';
+import { set } from 'property-seek';
+import { Failure } from '@quenk/preconditions';
 import { fromSchema } from '../../../src/schema/checks';
-import { Check, CheckF, TypeOf } from '../../../src/schema/checks/check';
+
+const schemas = {
+
+    string: {
+
+        type: 'string',
+
+    },
+    number: {
+
+        type: 'number'
+
+    },
+    boolean: {
+
+        type: 'boolean'
+
+    },
+    object: {
+
+        type: 'object',
+        properties: {
+
+            name: { type: 'string' },
+
+            age: { type: 'number' },
+
+            profile: {
+
+                type: 'sum',
+
+                variants: {
+
+                    main: {
+
+                        type: 'string'
+
+                    },
+
+                    avatar: {
+
+                        type: 'number'
+                    }
+                }
+
+            },
+            flags: {
+
+                type: 'object',
+                properties: {
+
+                    active: {
+
+                        type: 'boolean'
+
+                    },
+
+                    additionalProperties: { type: 'boolean' }
+
+                }
+
+            },
+
+            tags: {
+
+                type: 'array',
+
+                items: { type: 'string' }
+
+            },
+
+            contact: {
+
+                type: 'object',
+
+                additionalProperties: {
+
+                    type: 'string'
+                }
+            }
+
+        }
+
+    },
+
+    array: {
+
+        type: 'array',
+
+        items: { type: 'object', properties: { id: { type: 'number' } } }
+
+    },
+
+    sum: {
+
+        type: 'sum',
+
+        variants: {
+
+            string: { type: 'string' },
+
+            boolean: { type: 'boolean' },
+
+            number: { type: 'number' }
+
+        }
+
+    }
+
+};
+
+const samples = {
+
+    string: 'this is a string',
+
+    number: 12,
+
+    boolean: true,
+
+    object: {
+
+        name: 'Otis Juma',
+
+        age: 24,
+
+        profile: 1256,
+
+        flags: {
+
+            active: true,
+
+            locked: false,
+
+            outstanding: true
+
+        },
+
+        tags: ['user', 'active', 'unlocked'],
+
+        contact: {
+
+            home: '6995534',
+
+            email: 'me@meme.com',
+
+            fax: 'N/A'
+
+        }
+
+    },
+
+    array: [{ id: 1 }, { id: 12 }, { id: 24 }],
+
+    sum: true
+
+}
 
 describe('checks', () => {
 
-    it('fromSchema', () => {
+    describe('fromSchema', () => {
 
-        let fs = [];
+        it('should produce a preconditions for string types', () => {
 
-        fromSchema({ type: 'object' })
-            .run((c: CheckF<Check>) => { fs.push(c); return c.next; })
+            must((fromSchema(schemas.string)(samples.string)).takeRight())
+                .be(samples.string);
 
-        must(fs[0]).be.instanceOf(TypeOf);
+            must((fromSchema(schemas.string)(samples.number)).takeLeft())
+                .be.instanceOf(Failure);
 
-    });
+        })
 
-});
+        it('should produce a precondition for number types', () => {
+
+            must((fromSchema(schemas.number)(samples.number)).takeRight())
+                .be(samples.number);
+
+            must((fromSchema(schemas.number)(samples.string)).takeLeft())
+                .be.instanceOf(Failure);
+
+        })
+
+        it('should produce a preconditions for boolean types', () => {
+
+            must((fromSchema(schemas.boolean)(samples.boolean)).takeRight())
+                .be(samples.boolean);
+
+            must((fromSchema(schemas.boolean)(samples.string)).takeLeft())
+                .be.instanceOf(Failure);
+
+        })
+
+        it('should produce a precondition for object types', () => {
+
+            let wrong =
+                set('profile', {},
+                    set('contact.home', 12,
+                        set('flags.active', 'active', samples.object)));
+
+            must((fromSchema(schemas.object)(samples.object)).takeRight())
+                .eql(samples.object);
+
+            must((fromSchema(schemas.object)(wrong)).takeLeft().explain())
+                .eql({
+                    profile: 'isNumber',
+                    flags: {
+
+                        active: 'isBoolean'
+                    },
+                    contact: {
+                        home: 'isString'
+                    }
+                })
+        })
+
+        it('should produce a precondition for array types', () => {
+
+            must((fromSchema(schemas.array)(samples.array)).takeRight())
+                .eql(samples.array);
+
+            must((fromSchema(schemas.array)(samples.array[0])).takeLeft())
+                .be.instanceOf(Failure);
+
+        });
+
+
+        it('should produce a precondition for sum types', () => {
+
+            must((fromSchema(schemas.sum)(samples.string)).takeRight())
+                .eql(samples.string);
+
+            must((fromSchema(schemas.sum)(samples.number)).takeRight())
+                .eql(samples.number);
+
+            must((fromSchema(schemas.sum)(samples.boolean)).takeRight())
+                .eql(samples.boolean);
+
+            must((fromSchema(schemas.sum)(samples.object)).takeLeft())
+                .be.instanceOf(Failure);
+
+        });
+    })
+})
