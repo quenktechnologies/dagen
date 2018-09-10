cc9a7d2943c7ede309f8a91bd1fd3635d15926ea
Lasana Murray <lmurray@quenk.com>
2018-09-10 09:24:49 -0400

    refactor: add schema/definitions submodule

---
+++ src/schema/definitions/index.ts
@@ -0,0 +1,27 @@
+import { set, get } from 'property-seek';
+import { merge, reduce } from '@quenk/noni/lib/data/record';
+import { Result } from '@quenk/preconditions/lib/result';
+import { Schema, } from '../';
+import { evalUsages, pull } from './ref';
+
+/**
+ * Definition is a schema that can be re-used elsewhere in the document.
+ */
+export type Definition = Schema;
+
+/**
+ * Definitions 
+ */
+export interface Definitions {
+
+    [key: string]: Definition
+
+}
+
+export const resolve =
+    (defs: Definitions) => (schema: Schema): Result<Schema, Schema> =>
+        <Result<Schema, Schema>>(evalUsages(defs)(pull({})('')(schema)))
+            .map((wanted: Definitions) => reduce(wanted, schema, _resolve));
+
+const _resolve = (p: Schema, c: Schema, k: string): Schema =>
+    set(k, merge(<Definition>get(k, p), c), p);
---
+++ test/schema/definitions/index_test.ts
@@ -0,0 +1,207 @@
+import * as must from 'must/register';
+import { resolve } from '../../../src/schema/definitions';
+
+const defs = {
+
+    int: {
+
+        type: 'int'
+
+    },
+
+    name: {
+
+        type: 'object',
+
+        properties: {
+
+            first: {
+
+                type: 'string'
+
+            },
+            last: {
+
+                type: 'string'
+            }
+        }
+
+    },
+    age: {
+
+        type: 'number'
+
+    },
+
+    status: {
+
+        type: 'sum',
+
+        variants: {
+
+            active: {
+
+                type: 'boolean'
+
+            },
+            flagged: {
+
+                type: 'number'
+
+
+            }
+
+        }
+
+    },
+
+    string: {
+
+        type: 'string'
+
+    }
+
+}
+
+const $r = resolve(defs);
+
+describe('definitions', () => {
+
+    describe('resolve', () => {
+
+        it('should resolve definitions on objects', () => {
+
+            must($r({
+
+                type: 'object',
+                properties: {
+
+                    id: { type: 'number' },
+
+                    clan: { type: '#string' },
+
+                    name: { type: '#name' },
+
+                    age: { type: '#age' },
+
+                    flags: { type: 'array', items: { type: '#string' } },
+
+                    status: { type: '#status' }
+                },
+
+                additionalProperties: { type: '#int' }
+
+            }).takeRight()).eql({
+                'type': 'object',
+                'properties': {
+                    'id': {
+
+                        'type': 'number'
+
+                    },
+                    'clan': {
+                        'type': 'string'
+                    },
+                    'flags': {
+
+                        'type': 'array',
+                        'items': {
+
+                            'type': 'string'
+
+                        }
+
+                    },
+                    'name': {
+                        'type': 'object',
+                        'properties': {
+                            'first': {
+                                'type': 'string'
+                            },
+                            'last': {
+                                'type': 'string'
+                            }
+                        }
+                    },
+                    'age': {
+                        'type': 'number'
+                    },
+                    'status': {
+                        'type': 'sum',
+                        'variants': {
+                            'active': {
+                                'type': 'boolean'
+                            },
+                            'flagged': {
+                                'type': 'number'
+                            }
+                        }
+                    }
+                },
+                'additionalProperties': {
+
+                    type: 'int'
+
+                }
+            });
+
+        });
+
+        it('should resolve sum types', () => {
+
+            must($r({
+
+                type: 'sum',
+                variants: {
+                    person: {
+                        type: 'object',
+                        properties: {
+
+                            name: { type: '#name' }
+
+                        }
+
+                    },
+                    company: {
+
+                        type: 'object',
+
+                        additionalProperties: { type: '#int' }
+
+                    }
+                }
+
+            }).takeRight()).eql(                {
+                    'type': 'sum',
+                    'variants': {
+                        'person': {
+                            'type': 'object',
+                            'properties': {
+                                'name': {
+                                    'type': 'object',
+                                    'properties': {
+                                        'first': {
+                                            'type': 'string'
+                                        },
+                                        'last'
+                                            : {
+                                            'type': 'string'
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        'company': {
+                            'type': 'object',
+                            'additionalProperties': {
+                                'type': 'int'
+                            }
+                        }
+                    }
+                }  );
+
+        });
+    });
+
+});
+
